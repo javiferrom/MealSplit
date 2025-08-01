e3971ad07940df512ecae75d15cb112f869b88a4
jferrom <javierfernandezromero03@gmail.com>
2025-08-01 06:45:42 +0200

    Update docs for web

--- lib/screens/summary_screen.dart
+++ lib/screens/summary_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../models/diner.dart';
-import '../models/dish.dart';
 import '../providers/diners_provider.dart';
 import '../providers/dishes_provider.dart';
 
